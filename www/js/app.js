99fe5947b200f1603f3996b475d910dd109d62c6
ego <ego@hackermail.com>
2020-06-17 04:13:29 +0630

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -29,20 +29,14 @@ angular
 
 .config(function($stateProvider, $urlRouterProvider) {
 
-    // Ionic uses AngularUI Router which uses the concept of states
-    // Learn more here: https://github.com/angular-ui/ui-router
-    // Set up the various states which the app can be in.
-    // Each state's controller can be found in controllers.js
     $stateProvider
 
-    // setup an abstract state for the tabs directive
         .state('tab', {
         url: '/tab',
         abstract: true,
-        templateUrl: 'templates/tabs.html'
+        templateUrl: 'www/templates/tabs.html'
     })
 
-    // Each tab has its own nav history stack:
 
     .state('tab.dash', {
             url: '/dash',
@@ -110,7 +104,6 @@ angular
             }
         });
 
-    // if none of the above states are matched, use this as the fallback
     $urlRouterProvider.otherwise('/tab/movies');
 
 });
